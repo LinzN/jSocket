c710f486de5038a5785c0dd635c715e0364a9ab3
Niklas <niklas.linz@enigmar.de>
2025-02-19 18:29:28 +0100

    add copy

--- src/main/java/de/linzn/jSocket/core/ThreadTaskler.java
+++ src/main/java/de/linzn/jSocket/core/ThreadTaskler.java
@@ -1,16 +1,21 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
+/*
+ * Copyright (C) 2025. Niklas Linz - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the LGPLv3 license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the LGPLv3 license with
+ * this file. If not, please write to: niklas.linz@enigmar.de
+ *
+ */
 package de.linzn.jSocket.core;
 
 public interface ThreadTaskler {
-    void runThreadPoolExecutor(Runnable var1);
+    void runThreadPoolExecutor(Runnable runnable);
 
-    void runThreadExecutor(Thread var1);
+    void runThreadExecutor(Thread thread);
 
-    void runSingleThreadExecutor(Runnable var1);
+    void runSingleThreadExecutor(Runnable runnable);
 
     boolean isDebugging();
 }
--- src/main/java/de/linzn/jSocket/server/JServerConnection.java
+++ src/main/java/de/linzn/jSocket/server/JServerConnection.java
@@ -1,7 +1,13 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
+/*
+ * Copyright (C) 2025. Niklas Linz - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the LGPLv3 license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the LGPLv3 license with
+ * this file. If not, please write to: niklas.linz@enigmar.de
+ *
+ */
 
 package de.linzn.jSocket.server;
 
